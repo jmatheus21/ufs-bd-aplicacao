31ca4267eec19e77e3d3eb921ab9abc44c5e27db
ulissesjc <ulisses.c.2811@gmail.com>
2024-09-13 20:19:56 -0300

    alterei_Mecanico-Model

--- app/Models/Mecanico.php
+++ app/Models/Mecanico.php
@@ -7,5 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mecanico extends Model
 {
-    use HasFactory;
+    protected $table = 'mecanico';
+
+    protected $primaryKey = 'cpf';
+
+    public $incrementing = false;
+
+
+    // Campos que podem ser preenchidos via mass assignment
+    protected $fillable = [
+        'cpf',
+        'primeiro_nome',
+        'sobrenome', 
+        'email',
+        'telefone',
+        'escolaridade',
+        'data_nascimento',
+        'salario',
+        'admissao',
+        'validade',
+        'sexo',
+        'endereco',
+        'estado_civil',
+        'raca',
+        'licencas',
+        'habilidades'
+    ];
 }
